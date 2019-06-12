96564c173ceef0f5aeee170da3a9c60f093cca20
wulman16 <will.ulman@gmail.com>
2019-06-12 14:48:11 -0400

    insert test documents

--- mongodb.js
+++ mongodb.js
@@ -16,9 +16,71 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection(`users`).insertOne({
-      name: `Will`,
-      age: 25
-    });
+    db.collection(`recipes`).insertMany(
+      [
+        {
+          title: `Almond Butter Toast`,
+          time: `5 minutes`,
+          instructions: `Toast bread. Spread almond butter on one side. Enjoy!`
+        },
+        {
+          title: `Overnight Oats`,
+          time: `6 hours`,
+          instructions: `Pour oats, frozen berries, and soy milk into a single-serving container. Chill overnight.`
+        },
+        {
+          title: `Quesadilla`,
+          time: `10 minutes`,
+          instructions: `Heat skillet over medium. Sprinkle cheese on one side of tortilla and fold in half. Cook each side until golden brown.`
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log(`Unable to insert recipes!`);
+        }
+
+        console.log(result.ops);
+      }
+    );
+
+    db.collection(`ingredients`).insertMany(
+      [
+        {
+          name: `whole-wheat bread`,
+          amount: `1 slice`
+        },
+        {
+          name: `almond butter`,
+          amount: `2 Tbsp`
+        },
+        {
+          name: `rolled oats`,
+          amount: `1/2 cup`
+        },
+        {
+          name: `frozen berries`,
+          amount: `1/2 cup`
+        },
+        {
+          name: `soy milk`,
+          amount: `3/4 cup`
+        },
+        {
+          name: `tortilla`,
+          amount: `1`
+        },
+        {
+          name: `shredded cheese`,
+          amount: `1/2 cup`
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log(`Unable to insert ingredients!`);
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
