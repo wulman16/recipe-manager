b8aef7ff9dfe92a11c2e97ad5e6acb276b9955dd
wulman16 <will.ulman@gmail.com>
2019-06-13 14:00:46 -0400

    add basic validations to models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require(`mongoose`);
+const validator = require(`validator`);
 
 mongoose.connect(`mongodb://127.0.0.1:27017/recipe-manager-api`, {
   useNewUrlParser: true,
@@ -7,61 +8,97 @@ mongoose.connect(`mongodb://127.0.0.1:27017/recipe-manager-api`, {
 
 const User = mongoose.model(`User`, {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error(`Age must be a positive number!`);
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error(`Email is invalid!`);
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value.length < 7) {
+        throw new Error(`Password must be at least 7 characters!`);
+      } else if (validator.contains(value, `password`)) {
+        throw new Error(`Password cannot contain the word "password"!`);
+      }
+    }
   }
 });
 
-// const me = new User({
-//   name: `Will`,
-//   age: `whatttttt`
-// });
+const me = new User({
+  name: `   Will `,
+  email: `qwertyuiop161661@yahoo.com   `
+});
 
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch(error => {
-//     console.log(`Error: ${error}`);
-//   });
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch(error => {
+    console.log(`Error: ${error}`);
+  });
 
 const Recipe = mongoose.model(`Recipe`, {
   title: {
-    type: String
+    type: String,
+    required: true
   },
   time: {
-    type: String
+    type: String,
+    required: true
   },
   instructions: {
-    type: String
+    type: String,
+    required: true
   }
 });
 
 const Ingredient = mongoose.model(`Ingredient`, {
   name: {
-    type: String
+    type: String,
+    required: true
   },
   amount: {
     type: String
   },
   isVegan: {
-    type: Boolean
+    type: Boolean,
+    required: true
   }
 });
 
-const almondButterToast = new Recipe({
-  title: `Almond Butter Toast`,
-  time: 5,
-  instructions: `Toast bread. Spread almond butter on one side. Enjoy!`
-});
+// const almondButterToast = new Recipe({
+//   title: `Almond Butter Toast`,
+//   time: 5,
+//   instructions: `Toast bread. Spread almond butter on one side. Enjoy!`
+// });
 
-almondButterToast
-  .save()
-  .then(() => {
-    console.log(almondButterToast);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// almondButterToast
+//   .save()
+//   .then(() => {
+//     console.log(almondButterToast);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
