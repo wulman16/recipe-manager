b91ea2131ba04f2e8d582e9a2c86ee80b0aa90e3
wulman16 <will.ulman@gmail.com>
2019-06-13 13:38:41 -0400

    add models for Ingredient and Recipe

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -14,15 +14,54 @@ const User = mongoose.model(`User`, {
   }
 });
 
-const me = new User({
-  name: `Will`,
-  age: `whatttttt`
+// const me = new User({
+//   name: `Will`,
+//   age: `whatttttt`
+// });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch(error => {
+//     console.log(`Error: ${error}`);
+//   });
+
+const Recipe = mongoose.model(`Recipe`, {
+  title: {
+    type: String
+  },
+  time: {
+    type: String
+  },
+  instructions: {
+    type: String
+  }
 });
 
-me.save()
+const Ingredient = mongoose.model(`Ingredient`, {
+  name: {
+    type: String
+  },
+  amount: {
+    type: String
+  },
+  isVegan: {
+    type: Boolean
+  }
+});
+
+const almondButterToast = new Recipe({
+  title: `Almond Butter Toast`,
+  time: 5,
+  instructions: `Toast bread. Spread almond butter on one side. Enjoy!`
+});
+
+almondButterToast
+  .save()
   .then(() => {
-    console.log(me);
+    console.log(almondButterToast);
   })
   .catch(error => {
-    console.log(`Error: ${error}`);
+    console.log(error);
   });
