85edd066986246ce44241f17cf2f64bb606f3aa7
wulman16 <will.ulman@gmail.com>
2019-06-14 16:06:45 -0400

    write practice code for jwt auth

--- src/index.js
+++ src/index.js
@@ -16,17 +16,16 @@ app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
 
-const bcrypt = require(`bcryptjs`);
+const jwt = require(`jsonwebtoken`);
 
 const myFunction = async () => {
-  const password = `Red12345!`;
-  const hashedPassword = await bcrypt.hash(password, 8);
+  const token = jwt.sign({ _id: `abc123` }, `thisismynewcourse`, {
+    expiresIn: `10 seconds`
+  });
+  console.log(token);
 
-  console.log(password);
-  console.log(hashedPassword);
-
-  const isMatch = await bcrypt.compare(`ed12345!`, hashedPassword);
-  console.log(isMatch);
+  const data = jwt.verify(token, `thisismynewcourse`);
+  console.log(data);
 };
 
 myFunction();
