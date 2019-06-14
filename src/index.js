fe3be42d1094aa164f6fa81c657b97a40b2f7382
wulman16 <will.ulman@gmail.com>
2019-06-14 15:17:54 -0400

    practice password hashing with bcrypt

--- src/index.js
+++ src/index.js
@@ -15,3 +15,18 @@ app.use(recipeRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
+
+const bcrypt = require(`bcryptjs`);
+
+const myFunction = async () => {
+  const password = `Red12345!`;
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(hashedPassword);
+
+  const isMatch = await bcrypt.compare(`ed12345!`, hashedPassword);
+  console.log(isMatch);
+};
+
+myFunction();
