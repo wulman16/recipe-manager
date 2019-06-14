0fa0b78e5018bbdaeb8b664f2839621f99d86293
wulman16 <will.ulman@gmail.com>
2019-06-14 16:24:18 -0400

    remove jwt auth practice code

--- src/index.js
+++ src/index.js
@@ -15,17 +15,3 @@ app.use(recipeRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
-
-const jwt = require(`jsonwebtoken`);
-
-const myFunction = async () => {
-  const token = jwt.sign({ _id: `abc123` }, `thisismynewcourse`, {
-    expiresIn: `10 seconds`
-  });
-  console.log(token);
-
-  const data = jwt.verify(token, `thisismynewcourse`);
-  console.log(data);
-};
-
-myFunction();
