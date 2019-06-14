851ea8bdf78663bef6e5e22d51d2768ed6b38e40
wulman16 <will.ulman@gmail.com>
2019-06-14 12:41:48 -0400

    add practice code for async await functions

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -3,13 +3,27 @@ const Ingredient = require(`../src/models/ingredient`);
 
 //5d02a89ff0b5d0041b88b358
 
-Ingredient.findByIdAndDelete(`5d02a89ff0b5d0041b88b358`)
-  .then(() => {
-    return Ingredient.countDocuments({ isVegan: true });
+// Ingredient.findByIdAndDelete(`5d02a89ff0b5d0041b88b358`)
+//   .then(() => {
+//     return Ingredient.countDocuments({ isVegan: true });
+//   })
+//   .then(ings => {
+//     console.log(ings);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+const deleteIngredientAndCountVeganIngredients = async id => {
+  await Ingredient.findByIdAndDelete(id);
+  const count = await Ingredient.countDocuments({ isVegan: true });
+  return count;
+};
+
+deleteIngredientAndCountVeganIngredients(`5d02a135e90e4c016ad7e2bc`)
+  .then(count => {
+    console.log(count);
   })
-  .then(ings => {
-    console.log(ings);
-  })
-  .catch(error => {
-    console.log(error);
+  .catch(e => {
+    console.log(e);
   });
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -3,13 +3,27 @@ const User = require(`../src/models/user`);
 
 // 5d028e3f9eb406f7e64a099e
 
-User.findByIdAndUpdate(`5d029f494cb621fe42957411`, { age: 1 })
-  .then(user => {
-    console.log(user);
-    return User.countDocuments({ age: 1 });
-  })
-  .then(users => {
-    console.log(users);
+// User.findByIdAndUpdate(`5d029f494cb621fe42957411`, { age: 1 })
+//   .then(user => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 });
+//   })
+//   .then(users => {
+//     console.log(users);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount(`5d028e3f9eb406f7e64a099e`, 2)
+  .then(count => {
+    console.log(count);
   })
   .catch(error => {
     console.log(error);
