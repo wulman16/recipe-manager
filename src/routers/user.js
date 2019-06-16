2c9a0a9a862a69d446674d8104a97272d63a9a40
wulman16 <will.ulman@gmail.com>
2019-06-16 11:48:27 -0400

    fix typos in logout route

--- src/routers/user.js
+++ src/routers/user.js
@@ -27,9 +27,9 @@ router.post(`/users/login`, async (req, res) => {
   }
 });
 
-router.post(`/users/logout`, async (req, res) => {
+router.post(`/users/logout`, auth, async (req, res) => {
   try {
-    req.user.tokens = req.user.token.filter(token => {
+    req.user.tokens = req.user.tokens.filter(token => {
       return token.token !== req.token;
     });
     await req.user.save();
