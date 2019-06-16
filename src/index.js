8275a23eb9a5eee4efb7d4b5cf02dd09d4d4c5da
wulman16 <will.ulman@gmail.com>
2019-06-16 10:23:36 -0400

    stub out sample middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ const recipeRouter = require(`./routers/recipe`);
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  if (req.method === `GET`) {
+    res.send(`GET requests are disabled`);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(ingredientRouter);
