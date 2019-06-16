6864a915cf97d13a3f5f4f977b286e3fe0d7e6b1
wulman16 <will.ulman@gmail.com>
2019-06-16 14:01:21 -0400

    authenticate delete user endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -81,13 +81,10 @@ router.patch(`/users/:id`, async (req, res) => {
   }
 });
 
-router.delete(`/users/:id`, async (req, res) => {
-  const _id = req.params.id;
-
+router.delete(`/users/me`, auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(_id);
-    if (!user) return res.status(404).send();
-    res.send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch (e) {
     res.status(500).send();
   }
