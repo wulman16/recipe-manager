5026b23563db6046700b1dff6c927f4483053930
wulman16 <will.ulman@gmail.com>
2019-06-18 14:05:02 -0400

    customize file upload error messages

--- src/index.js
+++ src/index.js
@@ -21,9 +21,18 @@ const upload = multer({
   }
 });
 
-app.post(`/upload`, upload.single(`upload`), (req, res) => {
-  res.send();
-});
+app.post(
+  `/upload`,
+  upload.single(`upload`),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({
+      error: error.message
+    });
+  }
+);
 
 app.use(express.json());
 app.use(userRouter);
