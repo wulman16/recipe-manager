980e6e44c087ff7a11118809b75204c8bd7e18c0
wulman16 <will.ulman@gmail.com>
2019-06-18 13:50:32 -0400

    add test validation for file uploads

--- src/index.js
+++ src/index.js
@@ -8,8 +8,19 @@ const port = process.env.PORT || 3000;
 
 const multer = require(`multer`);
 const upload = multer({
-  dest: `images`
+  dest: `images`,
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error(`File must be a Word document!`));
+    }
+
+    cb(undefined, true);
+  }
 });
+
 app.post(`/upload`, upload.single(`upload`), (req, res) => {
   res.send();
 });
