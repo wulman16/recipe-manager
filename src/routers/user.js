836963af73bbce8790b2c3e730174677eb32ec72
wulman16 <will.ulman@gmail.com>
2019-06-18 15:01:19 -0400

    format and resize image uploads

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const User = require(`../models/user`);
 const auth = require(`../middleware/auth`);
 const router = new express.Router();
 const multer = require(`multer`);
+const sharp = require(`sharp`);
 
 router.post(`/users`, async (req, res) => {
   const user = new User(req.body);
@@ -97,12 +98,17 @@ const upload = multer({
     cb(undefined, true);
   }
 });
+
 router.post(
   `/users/me/avatar`,
   auth,
   upload.single(`avatar`),
   async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
   },
@@ -131,7 +137,7 @@ router.get(`/users/:id/avatar`, async (req, res) => {
       throw new Error();
     }
 
-    res.set("Content-Type", "image/jpg");
+    res.set("Content-Type", "image/png");
     res.send(user.avatar);
   } catch (e) {
     res.status(404).send();
